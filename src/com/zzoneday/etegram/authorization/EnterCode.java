45f1ed829597d35d736cab9162d92b46848ba234
zzoneday <azminseo@gmail.com>
2019-05-13 01:01:22 +0300

    Интерфейс начало; 1. Окно, продолжение работ с кнопкой, поиск лучшего варианта реализации 2. Пока забил на кнопку, создаю второе окно (Рисую рисую)

--- src/com/zzoneday/etegram/authorization/EnterCode.java
+++ src/com/zzoneday/etegram/authorization/EnterCode.java
@@ -6,8 +6,6 @@ import com.zzoneday.etegram.main.MainForm;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -17,42 +15,49 @@ public class EnterCode implements authorization {
     private JPanel rootPanel;
     private JPasswordField passwordField1;
     private JPanel logoPanel;
-    private JLabel textPleaseTypeNumber;
+    private JLabel mainText;
     private JPanel enterPhoneNumberJPanel;
     private JTextField textField1;
     private JPanel buttonJPanel;
     private JButton buttonNext;
+    private JPanel MainText;
+    private JLabel textYourNumber;
 
     private BufferedImage logo;
     private BufferedImage iconLock;
     private BufferedImage buttonBackground;
 
     EnterCode() {
+        //Основные пункты загрузки
         loadImage();
-
-
         rootPanel.setLayout(new BoxLayout(rootPanel, BoxLayout.Y_AXIS));
 
-        textPleaseTypeNumber.setText("<html><center>Введите код из сообщения что пришло вам на телефон блеать<center></html>");
-        textPleaseTypeNumber.setFont(Education.getCustomFont("regular", 16f));
-        textPleaseTypeNumber.setForeground(Color.WHITE);
+        //Основной текст
+        mainText.setText(
+                "<html><center>На данный номер телефона было отправлено\n SMS-сообщение с кодом подтверждения. " +
+                "Пожалуйста, введите этот код в поле ниже:<center></html>");
+        mainText.setFont(Education.getCustomFont("regular", 14f));
+        mainText.setForeground(Color.WHITE);
 
+        //Текст номера телефона пользователя
+        textYourNumber.setText("+7 900 707-92-29");
+        textYourNumber.setBackground(new Color(0, 0, 0, 0));
+        textYourNumber.setFont(Education.getCustomFont("light", 38f));
+        textYourNumber.setForeground(Color.lightGray);
+
+        //Форма для ввода кода
         passwordField1.setBackground(new Color(0, 0, 0, 0));
         passwordField1.setFont(Education.getCustomFont("regular", 34f));
         passwordField1.setForeground(Color.white);
         passwordField1.setBorder(null);
 
-
+        //Кнопка "Продолжить" для перехода в следующее окно
         buttonNext.setText("ПРОДОЛЖИТЬ");
         buttonNext.setFont(Education.getCustomFont("regular",20f));
         buttonNext.setForeground(Color.WHITE);
-
         buttonNext.setIcon(new ImageIcon(buttonBackground));
         buttonNext.setHorizontalTextPosition(0);
-
-
         buttonNext.addActionListener(e -> {
-
 
             //Безопасность на высоте
             String code = String.valueOf(passwordField1.getPassword());
@@ -96,7 +101,7 @@ public class EnterCode implements authorization {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 //(370-165)/2 = 102
-                g.drawImage(logo, 102, 40, null);
+                g.drawImage(logo, 102, 30, null);
             }
 
         };
@@ -105,19 +110,21 @@ public class EnterCode implements authorization {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.drawImage(iconLock, 10, 10, null);
+                g.drawImage(iconLock, 10+78, 10, null);
+
+                //156 ширина
 
                 Graphics2D twoD = (Graphics2D) g;
                 twoD.setColor(Color.WHITE);
                 twoD.setStroke(new BasicStroke(2));
-                twoD.drawLine(0,22+10+20, 370, 22+20+10);
+                twoD.drawLine(78,22+10+20, 292, 22+20+10);
             }
         };
     }
 
     private void loadImage() {
         try {
-            logo = ImageIO.read(new File("res/GUI Components/logo.png"));
+            logo = ImageIO.read(new File("res/GUI Components/logo-mini.png"));
             iconLock = ImageIO.read(new File("res/GUI Components/icon-lock.png"));
             buttonBackground = ImageIO.read(new File("res/GUI Components/button-background.png"));
         } catch (IOException e) {
